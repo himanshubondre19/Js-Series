725e9971f4a92c6c3b87da1bcdf48fd4d8f42a54
Himanshu Bondre <himanshu.bondre19@gmail.com>
2026-08-19 20:15:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+
+let name = "Sakshi"
+let age = 20
+let isLoggedIn = true
+
+// number -> 2 to the power 53
+// bigint 
+// string --> "Himanshu"
+// boolean --> true / false
+// undefined --> 
+// null --> standalone value
+// symbol --> unique
+// object 
+
+console.log(typeof "hitesh");
+console.log(typeof null);
