efdd9493f5d52f7f871fe620b9a9765e0aeb1175
Himanshu Bondre <himanshu.bondre19@gmail.com>
2026-08-20 18:27:56 +0000

    Operaions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,34 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber);
+
+
+// ************************** OPERATIONS ***************************
+let value = 22
+let negValue = -value
+
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2**2); // it means 2 to the power 2
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "Himanshu"
+let str2 = "Sakshi"
+let str3 = str1 + str2
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(2+ 1 + "2");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
